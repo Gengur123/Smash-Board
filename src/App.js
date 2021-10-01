dfac831f8110b4626c6c098e8b89382299bb2e57
Gengur123 <meleearthur@gmail.com>
2021-10-01 15:05:18 -0700

    Made the project more organized

--- src/App.js
+++ src/App.js
@@ -1,64 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import PlayingBlock from './components/PlayingBlock';
+import StandbyBlock from './components/StandbyBlock';
 
 function App() {
-  const [players, setPlayers] = useState([]);
-
-  useEffect(() => {
-    axios({
-      url: 'https://api.smash.gg/gql/alpha',
-      headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` },
-      method: 'post',
-      data: {
-        query: `
-          query query2 {
-            tournament(id: 320802) {
-              events {
-                phases {
-                  sets {
-                    nodes {
-                      slots {
-                        entrant {
-                          name
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-          `
-      }
-    }).then((result) => {
-      const tempPlayers = []
-      for (let i = 0; i < result.data.data.tournament.events.length; i++){
-        let event = result.data.data.tournament.events[i]
-        for (let phase of event.phases){
-          for (let nodes of phase.sets.nodes){
-              for (let slot of nodes.slots){
-                if (slot.entrant){
-                  tempPlayers.push(slot.entrant.name)
-                }
-              }
-          }
-        }
-      }
-      setPlayers(tempPlayers)
-    });
-  },[]);
-
-  
-  
 
   return (
-    <div className="App">
-      {
-        players.map((player ) => {
-        return (<p>{player}</p>)
-        })
-      }
-    </div>
+    <React.Fragment>
+      <StandbyBlock/>
+      <PlayingBlock/>
+    </React.Fragment>
     
   );
 }
