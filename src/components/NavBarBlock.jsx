a24b72d36d421ef0d9d78330060d1eb37917c9a2
Gengur123 <meleearthur@gmail.com>
2021-10-04 21:58:11 -0700

    Add stylilng

--- src/components/NavBarBlock.jsx
+++ src/components/NavBarBlock.jsx
@@ -5,10 +5,12 @@ import './NavBarBlock.css'
 function NavBarBlock () {
 
   return (
-    <div>
-      <img src={smash} alt="Smash"/>
-      NavBarBlock
-    </div>
+    <nav>
+      <div class="sort">
+        <img src={smash} alt="Smash"/>
+        <h1>Oregon Smash Dashboard</h1>
+      </div>
+    </nav>
   )
 }
 
