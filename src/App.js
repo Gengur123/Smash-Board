09ffd3b6f673f798555b60641f859996bc01475d
Gengur123 <meleearthur@gmail.com>
2021-10-05 21:22:15 -0700

    Reorganize structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PlayingBlock from './components/PlayingBlock';
 import StandbyBlock from './components/StandbyBlock';
 import DqBlock from './components/DqBlock';
-import NavBarBlock from './components/NavBarBlock'
+import NavBarBlock from './components/NavBarBlock';
+import axios from 'axios';
 
 function App() {
 
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: 'https://api.smash.gg/gql/alpha',
+      headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` },
+      method: 'post',
+      data: {
+        query: `
+        query query2 {
+          tournament(id: 320802) {
+            events {
+              sets(
+                page: 1,
+                perPage: 10
+              ){
+                nodes{
+                  slots{
+                    entrant{
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+          `
+      }
+    }).then((result) => {
+      const tempPlayers = []
+
+      let event = result.data.data.tournament.events[0]
+      console.log(event)
+      for (let node of event.sets.nodes){
+          let temp = []
+          for (let slot of node.slots){
+            if (slot.entrant){
+              temp.push(slot.entrant.name)
+            }
+          }
+          temp.push('S')
+          console.log(temp)
+          tempPlayers.push(temp)
+      }
+      console.log(tempPlayers)
+      setPlayers(tempPlayers)
+    });
+  },[]);
+  
   return (
     <React.Fragment>
       <NavBarBlock/>
       <div class="row">
         <div class="column">
-        <StandbyBlock/>
+        <StandbyBlock players={players}/>
         </div>
         <div class="column">
         <PlayingBlock/>
--- src/components/StandbyBlock.jsx
+++ src/components/StandbyBlock.jsx
@@ -1,66 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect} from 'react';
 import MatchBlock from './MatchBlock.jsx';
 
-function StandbyBlock () {
 
-  const [players, setPlayers] = useState([]);
+function StandbyBlock (props) {
+
+  console.log("PLAYER LIST APPJS")
+  console.log(props.players)
+  const [standbyPlayers, setStandbyPlayers] = useState(props.players);
 
   useEffect(() => {
-    axios({
-      url: 'https://api.smash.gg/gql/alpha',
-      headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` },
-      method: 'post',
-      data: {
-        query: `
-        query query2 {
-          tournament(id: 320802) {
-            events {
-              sets(
-                page: 1,
-                perPage: 10
-              ){
-                nodes{
-                  slots{
-                    entrant{
-                      name
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-          `
-      }
-    }).then((result) => {
-      const tempPlayers = []
-
-      let event = result.data.data.tournament.events[0]
-      console.log(event)
-      for (let node of event.sets.nodes){
-          let temp = []
-          for (let slot of node.slots){
-            if (slot.entrant){
-              temp.push(slot.entrant.name)
-            }
-          }
-          console.log(temp)
-          tempPlayers.push(temp)
-      }
-      console.log(tempPlayers)
-      setPlayers(tempPlayers)
-    });
-  },[]);
-
+    setStandbyPlayers(props.players);
+  }, [props.players])
   
-  
-
   return (
     <div className="App" id="standby">
       <h1>Standby</h1>
       {
-        players.map((player ) => {
+        standbyPlayers.map((player ) => {
         return (<MatchBlock
           p1={player[0]}
           p2={player[1]}
