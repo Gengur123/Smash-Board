d6709c82e9f2121dc89475ccb21d4aef9a4eeef1
Gengur123 <meleearthur@gmail.com>
2021-10-05 20:25:34 -0700

    Fix header to make it look more spaced

--- src/components/NavBarBlock.jsx
+++ src/components/NavBarBlock.jsx
@@ -6,10 +6,10 @@ function NavBarBlock () {
 
   return (
     <nav>
-      <div class="sort" id="nav">
+
         <img src={smash} alt="Smash"/>
         <h1>Oregon Smash Board</h1>
-      </div>
+        <img src={smash} alt="Smash"/>
     </nav>
   )
 }
